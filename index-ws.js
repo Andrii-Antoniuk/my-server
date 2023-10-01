e0b9bbdda51d5a9968eb43050998ddd3dfa0188f
Andrii Antoniuk <andrewantonuik@gmail.com>
2023-10-01 13:50:29 +0200

    Implemented simple db stuff

--- index-ws.js
+++ index-ws.js
@@ -19,6 +19,17 @@ const WebSocketServer = require('ws').Server;
 
 const wss = new WebSocketServer({ server: server });
 
+process.on('SIGINT', function () {
+    console.log('sigint');
+
+    wss.clients.forEach(function eachShutdown(client) {
+        client.close();
+    });
+
+    server.close(shutdownDb);
+});
+
+
 wss.on('connection', function connection(ws) {
     const numClients = wss.clients.size;
 
@@ -30,6 +41,10 @@ wss.on('connection', function connection(ws) {
         ws.send('Welcome to my server');
     }
 
+    db.run(`INSERT INTO visitors (count, time)
+        VALUES (${numClients}, datetime('now'))
+    `);
+
     ws.on('close', function close() {
         console.log('Client has disconnected');
     });
@@ -40,3 +55,31 @@ wss.broadcast = function broadcast(data) {
         client.send(data);
     });
 };
+
+/**
+ * DB
+ */
+
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(function () {
+    db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `);
+});
+
+function getCounts() {
+    db.each('SELECT * FROM visitors', function (err, row) {
+        console.log(row);
+    });
+}
+
+function shutdownDb() {
+    getCounts();
+    console.log('Shutting down db');
+    db.close();
+}
